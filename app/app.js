af1d9f348d7d4f79c5fd79a8a068e068073a562a
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-07-08 21:56:27 -0400

    trying functions with ajax

--- app/app.js
+++ app/app.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 
 	$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
     myData = data.Search;
-		$.each(myData, function(index, value) {
-			console.log(value.Title);
-		})
+	showResults();
   })
 });
+function showResults() {
+	$.each(myData, function(index, value) {
+			console.log(value.Title);
+		})
+}
