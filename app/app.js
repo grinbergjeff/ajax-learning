b28f1adb088b9e76b14fa213284580a6b2b73d51
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-07-10 16:52:02 -0400

    implemented get request

--- app/app.js
+++ app/app.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
 
-	$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
-    myData = data.Search;
-	showResults();
-  })
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+    });
+  });
+})
 });
+function getRequest(term) {
+	$.getJSON('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function(data){
+      showResults(data.Search);
+};
 function showResults(results) {
 	var html = '';
 	$.each(results, function(index, value) {
@@ -12,4 +20,4 @@ function showResults(results) {
 			console.log(value.Title);
 		})
 	$('#search-results').html(html);
-}
+};
